473ca4cc429a17a015d602215dcb386295eac4e4
jman5213 <jman5213@outlook.com>
2024-08-19 12:31:01 -0500

    Formatted for better readablility, next will solve 2 answer problem

--- PrimeCalcFast.py
+++ PrimeCalcFast.py
@@ -1,21 +1,34 @@
 from math import sqrt
 from time import perf_counter
 
-calcTo=int(input("calculate the nth prime #: "))
-currentNum=1
-primesFound=0
-time1=perf_counter()
+calcTo: int = int(input("Calculate the nth prime:"))
+primesFound: int = 2
+currentNum: int = 6
+time1 = perf_counter()
 
-def prime(num):
-  for div in range(3,int(sqrt(num))+1,2):
-    if num%div == 0:
-      return False
-  return True
-  
-while primesFound<calcTo:
-  if prime(currentNum):
-    primesFound+=1
-  currentNum+=2
 
-time2 = perf_counter()
-print('Your #:',str(currentNum-2)+'\nTime:',str(time2 -time1)+"sec")
+def prime(num: int) -> bool:
+    """
+    Returns a bool representing the primality of the argument
+
+    :num: a positive integer that follows the pattern 6n+/-1
+    """
+    for div in range(5, int(sqrt(num))+1, 6):
+        if num % div == 0 or num % (div+2) == 0:
+            return False
+    return True
+
+
+def main():
+    global primesFound, currentNum
+    while primesFound < calcTo:
+        primesFound += prime(currentNum-1) + prime(currentNum+1)
+        currentNum += 6
+
+    time2 = perf_counter()
+    print(f'Your #: {currentNum-7 if primesFound == calcTo else currentNum-5} \
+          \nTime: {time2-time1}sec')
+
+
+if __name__ == "__main__":
+    main()
